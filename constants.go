85ebc2ee0c995d8dfe1d7625bddc6cfd6bbe2156
Daniel Winterstein <daniel.winterstein@n-able.com>
2026-01-11 23:30:32 +0000

    tests

--- constants.go
+++ constants.go
@@ -8,7 +8,7 @@ const (
 
 	// Version is the version of the AIQA client library
 	// This is automatically updated by set-version-json.sh
-	Version = "0.4.9"
+	Version = "0.5.0"
 
 	// LogTag is used in all logging output to identify AIQA messages
 	LogTag = "AIQA"
